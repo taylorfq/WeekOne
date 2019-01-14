168aa5c56749275e739e1bc4f1ea9ab81ee88934
Aaron <aarontaylor@aarons-macbook-pro.local>
2019-01-14 17:58:27 -0500

    Initial Commit

---
+++ WeekOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeekOne
+//
+//  Created by Aaron Taylor on 2019-01-14.
+//  Copyright © 2019 Aaron Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
